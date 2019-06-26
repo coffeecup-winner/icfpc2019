509964cc87635b4a220b2ec56096e88184b6b73a
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2019-06-25 22:24:37 -0700

    Add problem trait

--- src/core.rs
+++ src/core.rs
@@ -8,7 +8,13 @@ trait State<A: Action, R: ReverseAction> {
     fn unapply_one(agent_id: u32, reverse_action: R);
 }
 
+trait Problem<S: State<A, R>, A: Action, R: ReverseAction> {
+    fn load_state(data: Vec<u8>) -> S;
+    fn save_solution(solution: Vec<A>) -> Vec<u8>;
+    fn score_solution(solution: Vec<A>) -> u64;
+}
+
 trait Strategy<S: State<A, R>, A: Action, R: ReverseAction> {
-    fn run(state: S, callback: dyn Fn(Vec<A>) -> ());
     fn name() -> String;
+    fn run(state: S, callback: dyn Fn(Vec<A>) -> ());
 }
